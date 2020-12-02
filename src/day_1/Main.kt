9c0b550f486c9051a96fd6ce6c2c2fc1ebcf92ce
msins <m.sinko2@hotmail.com>
2020-12-02 15:52:01 +0100

    Add day 2

--- src/day_1/Main.kt
+++ src/day_1/Main.kt
@@ -3,7 +3,7 @@ package day_1
 import java.io.File
 
 fun main() {
-    val nums = File("src/task_1/input.txt").readLines().map { it.toInt() }.toIntArray()
+    val nums = File("src/day_1/input.txt").readLines().map { it.toInt() }.toIntArray()
     val targetNum = 2020
 
     for (i in nums.indices) {
